bb2a75a906e5664716b78b2a911fb1274c3fe64c
Nick Carducci <nmcarducci@gmail.com>
2025-04-12 11:42:46 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { app, input } = require('@azure/functions');
 const sqlInputGeneralLedger = input.sql({
     commandText: 'select [Date], [Description], [Amount], [Category], [Platform], [LinkedAccount], [CreatedAt] from dbo.GeneralLedger',
     commandType: 'Text',
-    connectionStringSetting: `Driver={ODBC Driver 18 for SQL Server};Server=tcp:raiautomay.database.windows.net,1433;Database=RAIFinance;Uid=dumbcult;Pwd=${process.env.PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;`,
+    connectionStringSetting: "SqlConnectionString"//`Driver={ODBC Driver 18 for SQL Server};Server=tcp:raiautomay.database.windows.net,1433;Database=RAIFinance;Uid=dumbcult;Pwd=${process.env.PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;`,
 });
 
 app.http('generalledger', {
