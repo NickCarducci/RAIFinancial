0721c1066a0a828a86a3fdd48490e65e84313781
Nick Carducci <nmcarducci@gmail.com>
2025-05-09 12:26:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -190,8 +190,8 @@ const configuration = new Configuration({
     basePath: 'https://production.plaid.com',//PlaidEnvironments.production,
     baseOptions: {
         headers: {
-            'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
-            'PLAID-SECRET': process.env.PLAID_SECRET,
+            'PLAID-CLIENT-ID': "",//process.env.PLAID_CLIENT_ID,
+            'PLAID-SECRET': "",//process.env.PLAID_SECRET,
             'Plaid-Version': '2020-09-14',
         },
     },
