c6e30d26bda10b8ce23455b5dd8a28bce3014af2
Nick Carducci <nmcarducci@gmail.com>
2025-05-12 16:39:47 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -203,10 +203,10 @@ const client = new PlaidApi(configuration);
 const getError = (error) => {
     let e = error;
     if (error.response) {
-        e = error.response.data;                   // data, status, headers
-        if (error.response.data && error.response.data.error) {
+        e = error.response//.data;                   // data, status, headers
+        /*if (error.response.data && error.response.data.error) {
             e = error.response.data.error;           // my app specific keys override
-        }
+        }*/
     } else if (error.message) {
         e = error.message;
     } else {
